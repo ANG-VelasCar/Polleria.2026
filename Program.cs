2b67e8ea2d9aee41ecf1a32bd0c7218ef390ac07
ANG-VelasCar <71444757@continental.edu.pe>
2026-09-24 11:26:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "API Pollería activa");
+
+app.MapGet("/api/polleria", () =>
+{
+    return Results.Ok(new[]
+    {
+        new
+        {
+            id = 1,
+            codigo = "P001",
+            nombre = "Pollo a la brasa"
+        },
+        new
+        {
+            id = 2,
+            codigo = "P002",
+            nombre = "Pollo broaster"
+        }
+    });
+});
+
+app.Run();
